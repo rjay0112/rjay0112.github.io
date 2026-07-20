690813c3f5ade6c76b881d0874377b63ad2eee5f
rjay0112 <ryanwjay@gmail.com>
2026-07-19 20:37:09 -0400

    added details to sidebar
    Added list items and month logic, real events still need to be added in

--- floorplan/code.js
+++ floorplan/code.js
@@ -11,10 +11,31 @@
 //[DONE]resize section
 //make it look a bit nicer
 //Add important dates calendar
-//dark mode
+//[DONE]dark mode
+//allow for multi dates
+//add in real dates and plans
+//adjust mappings
+//adjust urgency colours
 
-cleaning_durations = 
-[
+important_dates = [
+  [["July", 28, 2026], "Lorem ipsum dolor sit amet, consectetur adipiscing 1", "Event"],
+  [["July", 29, 2026], "Lorem ipsum dolor sit amet, consectetur adipiscing 2", "Bday"],
+  [["July", 30, 2026], "Lorem ipsum dolor sit amet, consectetur adipiscing 3", "Trip"],
+  [["July", 31, 2026], "Lorem ipsum dolor sit amet, consectetur adipiscing 4", "Event"],
+  [["Aug", 1, 2026], "Lorem ipsum dolor sit amet, consectetur adipiscing 5", "Event"],
+  [["Aug", 2, 2026], "Lorem ipsum dolor sit amet, consectetur adipiscing 6", "Bday"],
+  [["Aug", 4, 2026], "Lorem ipsum dolor sit amet, consectetur adipiscing 7", "Event"],
+  [["Aug", 6, 2026], "Lorem ipsum dolor sit amet, consectetur adipiscing 8", "Bday"],
+  [["Aug", 9, 2026], "Lorem ipsum dolor sit amet, consectetur adipiscing 9", "Event"],
+  [["Aug", 12, 2026], "Lorem ipsum dolor sit amet, consectetur adipiscing 10", "Event"],
+  [["Sept", 2, 2026], "Lorem ipsum dolor sit amet, consectetur adipiscing 11", "Event"],
+  [["Sept", 3, 2026], "Lorem ipsum dolor sit amet, consectetur adipiscing 12", "Bday"],
+  [["Sept", 5, 2026], "Lorem ipsum dolor sit amet, consectetur adipiscing 13", "Event"],
+  [["Sept", 8, 2026], "Lorem ipsum dolor sit amet, consectetur adipiscing 14", "Event"],
+
+]
+
+cleaning_durations = [
   ["bedroom_floor",168],
   ["bedroom_sheets",720],
   ["master_bath_garbage",336],
@@ -58,8 +79,10 @@ cleaning_durations =
     }
   }
   colourCheck()
+  populateUpcomingList()
+  populateCalendar()
 
-setInterval(colourCheck,60000)
+setInterval(refreshData,60000)
 
 locations = document.getElementsByClassName("location")
 for (var i=0;i<locations.length;i++){
@@ -76,6 +99,11 @@ function resetTimer(){
   document.getElementsByClassName("warning")[0].style.opacity=0
 }
 
+function refreshData(){
+  colourCheck()
+  populateCalendar()
+  populateUpcomingList()
+}
 
 function colourCheck(){
   console.log("check")
@@ -87,4 +115,108 @@ function colourCheck(){
     opacity_shift = elapsed/cur_values[0]
     locations[i].style.opacity=opacity_shift
   }
+}
+//13 things
+function populateUpcomingList(){
+  listItems = document.getElementsByClassName("list_view")[0]
+  console.log(listItems)
+  for (var i=0;i<important_dates.length;i++){
+    if(important_dates[i][0][0]=="July"){
+      for (var j=0;j<listItems.children.length;j++){
+      //for (const event_child of listItems.children){
+        console.log("Test")
+        event_child=listItems.children[j]
+        console.log(event_child.children[0])
+        e_date = event_child.children[0]
+        e_text = event_child.children[1]
+        console.log(e_date)
+        e_date.style.borderRadius = "4px"
+        if(important_dates[i+j][2]=="Bday"){
+          e_date.style.backgroundColor = "#181800"
+        }
+        else if(important_dates[i+j][2]=="Event"){
+          e_date.style.backgroundColor = "#001818"
+        }
+        else if(important_dates[i+j][2]=="Trip"){
+          e_date.style.backgroundColor = "#001800"
+        }
+        e_date.children[0].innerHTML=important_dates[i+j][0][0]
+        e_date.children[1].innerHTML=important_dates[i+j][0][1]
+        e_text.innerHTML=important_dates[i+j][1]
+        console.log(event_child.children[0].children[0])
+        console.log(event_child)
+      }
+    break
+
+    }
+  }
+
+
+}
+
+function populateCalendar(){
+  first_of_month = new Date()
+  first_of_month.setDate(first_of_month.getDate()-(first_of_month.getDate()-1))
+  calendar = document.getElementsByClassName("month_cal")
+  //console.log(calendar)
+  for (var i=0;i<calendar.length;i++){
+    let set_month= first_of_month
+    set_month.setMonth(set_month.getMonth()+i)
+
+    calendar[i].children[0].innerHTML=month_to_text(set_month.getMonth())[0]
+    //console.log(month_to_text(set_month.getMonth())[0])
+    month_short = month_to_text(set_month.getMonth())[1]
+    //left off having start day of week and number of days in month
+    //need to iterate through rows of tables
+    cal_table = calendar[i].children[1].children[0]
+    day_count=1-first_of_month.getDay()
+    //console.log(day_count)
+    //console.log(set_month)
+    days_in_month = daysInMonth(set_month.getMonth()+1,set_month.getFullYear())
+    //console.log(days_in_month)
+    for (var j=1;j<cal_table.children.length;j++){
+      for (var k=0;k<cal_table.children[j].children.length;k++){
+        cal_table.children[j].children[k].style.backgroundColor="#000000" 
+        if(day_count>0 && day_count<=days_in_month){
+          cal_table.children[j].children[k].innerHTML=day_count
+          for (var l=0;l<important_dates.length;l++){
+            if(important_dates[l][0][0]==month_short && important_dates[l][0][1]==day_count){
+              if(important_dates[l][2]=="Bday"){
+                cal_table.children[j].children[k].style.backgroundColor="#181800"
+              }else if(important_dates[l][2]=="Event"){
+                cal_table.children[j].children[k].style.backgroundColor="#001818"
+              }else if(important_dates[l][2]=="Trip"){
+                cal_table.children[j].children[k].style.backgroundColor="#000018"
+              }
+            }
+          }
+        }else{
+          cal_table.children[j].children[k].innerHTML=""
+        }
+        day_count++
+
+      }
+    }
+
+
+  }
+}
+
+function month_to_text(month_text){
+  if(month_text==0){return ["January","Jan"]}
+  else if(month_text==1){return ["February","Feb"]}
+  else if(month_text==2){return ["March","Mar"]}
+  else if(month_text==3){return ["April","Apr"]}
+  else if(month_text==4){return ["May","May"]}
+  else if(month_text==5){return ["June","June"]}
+  else if(month_text==6){return ["July","July"]}
+  else if(month_text==7){return ["August","Aug"]}
+  else if(month_text==8){return ["September","Sept"]}
+  else if(month_text==9){return ["October","Oct"]}
+  else if(month_text==10){return ["November","Nov"]}
+  else if(month_text==11){return ["December","Dec"]}
+}
+
+function daysInMonth (month, year) {
+    return new Date(year, month, 0).getDate();
 }
